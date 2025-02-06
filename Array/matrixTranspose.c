f91c5415849eb0f93ab483b1ad8a0e26c5d085bc
Balaji Bandgar <balajibandgar26@gmail.com>
2025-02-06 17:50:05 +0530

    Adding Array file

---
+++ Array/matrixTranspose.c
@@ -0,0 +1,42 @@
+// Write a program to Transpose a matrix
+#include<stdio.h>
+void main(){
+    int rows, cols;
+    printf("Enter rows and colunms of the matrix :");
+    scanf("%d%d",&rows,&cols);
+
+    int matrix[rows][cols], transpose[cols][rows];
+
+    //initialize matrix
+    printf("Enter the elements of the matrix :\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    //Transpose of matrix
+    for (int i = 0; i < cols; i++)
+    {
+        for (int j = 0; j < rows; j++)
+        {
+            transpose[i][j] = matrix[j][i];
+        }
+    }
+    
+
+     printf("Transposed matrix:\n");
+     for (int i = 0; i < rows; i++)
+     {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d\t",transpose[i][j]);
+        }
+        printf("\n");
+     }
+     
+
+    
+}
